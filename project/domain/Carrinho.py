31552caa89952d28ee7a1cf5b6f8d9e679962e9d
William Ferreira Saraiva <williamferreirasr@gmail.com>
2026-09-18 19:33:43 -0300

    Fixed ProdutoCarrinho class, putting to_dict function and adding getter and setter to quantidade. Added 2 methods for Carrinho for now, they are adicionar and remover, but remover is not fully implemented

--- project/domain/Carrinho.py
+++ project/domain/Carrinho.py
@@ -1,12 +1,26 @@
 from __future__ import annotations
-from typing import Any
+from typing import Any, Optional
 from Produto import Produto
 from ProdutoCarrinho import ProdutoCarrinho
 
 class Carrinho:
   def __init__(self):
-    self._produto_carrinho:ProdutoCarrinho = []
+    self._produto_carrinho:list[ProdutoCarrinho] = []
   
-  
+  def adicionar_produto(self, produto:Produto):
+    pass
+
+  def remover_produto(self, produto:Produto, id_produto:Optional[int] = None):
+    if (not isinstance(produto, Produto)):
+      raise TypeError(f"Informe um produto de corretamente")
+    if(not isinstance(id_produto, int)):
+      raise TypeError(f"O id do produto é inválido")
+    if(id_produto is not None and produto.id_prod == id_produto):
+      self._produto_carrinho.remove(produto)
+    else:
+      if (produto in self._produto_carrinho):
+        self._produto_carrinho.remove(produto)
+      else:
+        raise ValueError(f"Objeto não encontrado")
 
   
--- project/domain/ProdutoCarrinho.py
+++ project/domain/ProdutoCarrinho.py
@@ -1,11 +1,37 @@
-from __future__ import annotations
-from Carrinho import Carrinho
 from Produto import Produto
+from typing import Any
 
 class ProdutoCarrinho:
   def __init__(self, produto:Produto,quantidade:int):
-    self.produto = produto
+    if (not isinstance(produto, Produto)):
+      raise TypeError(f"A entrada não é um produto valido")
+    self._produto = produto
     self.quantidade = quantidade
   
+  def to_dict(self) -> dict[str, Any]:
+    return {
+      "produto": self._produto.to_dict(),
+      "quantidade": self._quantidade,
+      "subtotal": self.subtotal_produtos()
+        }
+  
   def subtotal_produtos(self) -> float:
-    return self.produto.preco * quantidade
+    return self._produto.preco * self._quantidade
+
+  @property
+  def produto(self) -> Produto:
+    return self._produto
+
+  @property
+  def quantidade(self) -> int:        
+    return self._quantidade
+
+  @quantidade.setter
+  def quantidade(self, quantidade: int) -> None:
+    if(not isinstance(quantidade, int)):
+      raise TypeError(f"A quantidade deve ser um numero inteiro")
+    if (quantidade <= 0):
+      raise ValueError(f"A quantidade de compra nao pode ser inferior ou igual a 0")
+    if(quantidade > self.produto.quantidade):
+      raise ValueError(f"A quantidade de compra e superior ao estoque")
+    self._quantidade = quantidade
