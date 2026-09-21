364a3a69a2a2735036ecf780ee5ff902057fc255
William Ferreira Saraiva <williamferreirasr@gmail.com>
2026-09-21 10:27:47 -0300

    Created a new connection file(the old one will remain until all changes are did. Created a new folder called models, who will hold models for the sqlalchemy. Add sqlalchemy library)

--- project/database/connection.py
+++ project/database/connection.py
@@ -1,11 +1,20 @@
-import psycopg
-from os import getenv
 from dotenv import load_dotenv
+from os import getenv
+
+from sqlalchemy.exc import InterfaceError, DatabaseError
+
 
 load_dotenv(r"project\.env")
-
 def iniciar_conexao():
   try:
-    pass
-  except:
-    pass
+    engine = create_engine(f"postgresql+psycopg://{getenv(BD_USR)}:{getenv(DB_PORT)}@{getenv(DB_HOST)/{getenv(DB_NAME)}}/")
+
+  except InterfaceError as e:
+    print(f"Ocorreu um erro de interface: {e}")
+  
+  except DatabaseError as e:
+    print(f"Ocorreu um erro no banco de dados: {e}")
+
+  except Exception as e:
+    print(f"Ocorreu um erro inesperado: {e}")
+
