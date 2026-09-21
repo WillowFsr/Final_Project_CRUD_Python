3e250fb1422ffb0cafa7d0de5a8bb8417378fe9c
William Ferreira Saraiva <williamferreirasr@gmail.com>
2026-09-21 17:23:27 -0300

    Add base connection code on connection.py with the new library. Add models folder and the models for each entity in database

--- project/database/connection.py
+++ project/database/connection.py
@@ -1,20 +1,26 @@
 from dotenv import load_dotenv
 from os import getenv
-
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import InterfaceError, DatabaseError
 
-
 load_dotenv(r"project\.env")
-def iniciar_conexao():
+
+
+try:
+  engine = create_engine(f"postgresql+psycopg://{getenv('BD_USR')}:{getenv('DB_PASSWORD')}@{getenv('DB_HOST')}:{getenv('DB_PORxT')}/{getenv('DB_NAME')}")
+except (InterfaceError, DatabaseError) as e:
+  print(f"Ocorreu um erro: {e}")
+except Exception as e:
+  print(f"Ocorreu um erro inesperado: {e}")
+
+Session = sessionmaker(engine)
+
+@contextmanager
+def get_connection():
+  session = Session()
   try:
-    engine = create_engine(f"postgresql+psycopg://{getenv(BD_USR)}:{getenv(DB_PORT)}@{getenv(DB_HOST)/{getenv(DB_NAME)}}/")
-
-  except InterfaceError as e:
-    print(f"Ocorreu um erro de interface: {e}")
-  
-  except DatabaseError as e:
-    print(f"Ocorreu um erro no banco de dados: {e}")
-
-  except Exception as e:
-    print(f"Ocorreu um erro inesperado: {e}")
-
+    pass
+  finally:
+    session.close()
